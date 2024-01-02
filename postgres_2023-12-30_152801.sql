31e9056527673b7f0a7aa95de109e1e562599ff6
Yaroslav Krutko <krutkayaroslav@gmail.com>
2024-01-03 01:50:47 +0200

    add scheme

--- postgres_2023-12-30_152801.sql
+++ postgres_2023-12-30_152801.sql
@@ -1,2 +1,16 @@
-CREATE DATABASE postgres
-CREATE TABLE users (usersid INTEGER, username varchar (50), userpassword varchar(50));
+--create db
+CREATE DATABASE IF NOT EXISTS postgres;
+
+--select db
+USE  postgres;
+
+--create table
+CREATE TABLE IF NOT EXISTS users (
+    users_id INT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL
+);
+
+--insert data
+INSERT INTO users (users_id, username, password)
+VALUES (1, Rick, 12355);
